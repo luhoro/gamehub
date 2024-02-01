2a1a2c53abac80bf484a560e0ae1b22f8d937c8c
luhoro <luisashorowitz@gmail.com>
2024-01-31 22:37:57 -0300

    Prepare: Clean base default

---
+++ src/components/header/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='max-w-screen-lg h-28 px-2 '>
+      <p>Header babe</p>
+    </header>
+  )
+}
+
+export default Header
